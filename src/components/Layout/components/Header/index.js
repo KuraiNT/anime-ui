ef4c10af5ce92fadb2837adc173bd104a1792784
KuraiNT <89431659+KuraiNT@users.noreply.github.com>
2022-10-30 14:39:14 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,3 +1,5 @@
+import { faChevronDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
@@ -7,9 +9,61 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <h2>Header</h2>
-                {/* Logo */}
-                {/* Search */}
+                <img
+                    src="http://cdn.animevietsub.cc/data/logo/logoz.png"
+                    className={cx('logo')}
+                    alt="Anime Vietsub"
+                ></img>
+                <nav className={cx('nav')}>
+                    <ul className={cx('nav-list')}>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link', 'active')}>
+                                TRANG CHỦ
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                DẠNG ANIME
+                                <FontAwesomeIcon icon={faChevronDown} className={cx('down-icon')} />
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                TOP ANIME
+                                <FontAwesomeIcon icon={faChevronDown} className={cx('down-icon')} />
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                THỂ LOẠI
+                                <FontAwesomeIcon icon={faChevronDown} className={cx('down-icon')} />
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                SEASON
+                                <FontAwesomeIcon icon={faChevronDown} className={cx('down-icon')} />
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                THƯ VIỆN
+                            </a>
+                        </li>
+                        <li className={cx('nav-item')}>
+                            <a href="" className={cx('nav-link')}>
+                                LỊCH CHIẾU
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={cx('search')}>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                    <input placeholder="Tìm: tên tiếng nhật, anh, việt" className={cx('search-input')} />
+                </div>
+                <button>Dang nhap</button>
             </div>
         </header>
     );
